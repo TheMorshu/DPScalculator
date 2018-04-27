64cf217c648ba8611ae8c8df6f4c2ab529c266d0
Ilmari Heiskanen <ilmari_heiskanen@hotmail.com>
2018-04-27 20:00:20 +0300

    fix

--- src/main/java/fi/TheMorshu/dpscalculator/MainUI/MainUI.java
+++ src/main/java/fi/TheMorshu/dpscalculator/MainUI/MainUI.java
@@ -16,15 +16,12 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  *
  * @author ilmar
  */
-
-
 
 public class MainUI extends Application {
     
